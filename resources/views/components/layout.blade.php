a16fb15ffaef842fd0e0e9b0c681ed504c546f67
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-07-06 12:00:27 +0545

    added tailwind support

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,7 +2,7 @@
 
 <title>CMS</title>
 <link rel="stylesheet" href="{{ URL::asset ('css/app.css') }}">
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+@vite('resources/css/app.css')
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
