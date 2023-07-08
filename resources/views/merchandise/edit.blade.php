bf19657676c1348ffe0ac3e6e25cd22b3f146deb
RajaramBhurtel <sabinbhurtel@gmail.com>
2023-07-08 22:52:42 +0545

    merchandise view

---
+++ resources/views/merchandise/edit.blade.php
@@ -0,0 +1,54 @@
+<x-layout >
+    <x-main title="Edit Merchandise">
+        
+          <x-panel>
+            <div class="left-0  flex  justify-center bg-gray-200">
+              <div class="bg-white rounded shadow-lg p-8">
+                <div class="text-xl font-bold mb-4 text-center">EDIT</div>
+                <form id="merchandise_update" method="post" action="/merchandise/{{$merchandise->id}}/update">
+                  @csrf
+                  @method('PATCH')
+                  <table class="font-bold" style="font-size: 13px;">
+                    <tbody>
+                      <tr><td colspan="3"></td></tr><tr><td colspan="3"></td></tr><tr><td colspan="3"></td></tr><tr><td colspan="3"></td></tr>
+                      <tr>
+                        <td>
+                          Merchandise Code&nbsp;<b class="">*</b>
+                        </td>
+                        <td><span class="ml-1">:</span></td>
+                        <td>
+                          <input type="text" name="merchandise_code" id="merchandise_code" placeholder="Merchandise Code" class="ml-2 w-300 h-10 text-blue-500" value="{{$merchandise->merchandise_code}}">
+                        </td>
+                        @error('merchandise_code')
+                          <p class="text-red-500">{{ $message }}</p>
+                        @enderror
+                      </tr>
+                      <tr>
+                        <td>
+                          Merchandise Name&nbsp;<b class="street">*</b>
+                        </td>
+                        <td><span class="ml-1">:</span></td>
+                        <td>
+                          <input type="text" name="name" id="name" placeholder="Merchandise Name" class="ml-2 w-300 h-10 text-blue-500" value="{{$merchandise->name}}">
+                        </td>
+                        @error('name')
+                          <p class="text-red-500">{{ $message }}</p>
+                        @enderror
+                      </tr>
+                    </tbody>
+                  </table>
+                  <br>
+                  <div class="text-center">
+                    
+                  <button class="bg-indigo-600  hover:bg-indigo-800 text-white uppercase text-sm font-semibold px-4 py-2 rounded"> 
+                    <x-component.icons name="fa fa-save" /> Update</button>
+
+                  </div>
+                </form>
+                
+              </div>
+            </div>
+          </x-panel> 
+    </x-main >
+</x-layout >
+    
