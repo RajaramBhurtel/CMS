507dd832b536b3df25c205a044ad8e79c4b9c323
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-07-10 17:37:44 +0545

    fixes

--- public/js/map.js
+++ public/js/map.js
@@ -3,11 +3,15 @@ const autocompleteInput = new autocomplete.GeocoderAutocomplete(
     '3082d9f8f08846a987a1c0a3ed4ecd05', 
     {  });
 autocompleteInput.on('select', (location) => {
-console.log(location)
 const placeName = location.properties.address_line1;
 const placeLocation = location.properties.address_line2;
-const latitude = location.geometry.coordinates[1];
-const longitude = location.geometry.coordinates[0];
+const latitude = location.properties.lat;
+const longitude = location.properties.lon;
+
+if (!placeName || !placeLocation || !latitude || !longitude) {
+    alert("Invalid address. Please select a valid address.");
+    return;
+}
 
 // Set the selected location values to hidden inputs in the form
 document.getElementById('address_1').value = placeName;
