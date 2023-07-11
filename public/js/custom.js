519dacc12308eb257d24b7a5c98cc0344e7e6cc5
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-07-11 15:48:37 +0545

    autocomplete js fixes for multiple inputs

--- public/js/custom.js
+++ public/js/custom.js
@@ -17,29 +17,81 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Auto complete the location using geopapify
     
-    if (document.getElementById('autocomplete')) {
-      const autocompleteInput = new autocomplete.GeocoderAutocomplete(
-                                document.getElementById("autocomplete"), 
-                                '3082d9f8f08846a987a1c0a3ed4ecd05', 
-                                {  });
-        autocompleteInput.on('select', (location) => {
+    // if (document.getElementById('autocomplete') || document.getElementById('autocomplete_1')) {
+    //   const autocompleteInput = new autocomplete.GeocoderAutocomplete(
+    //                             document.getElementById("autocomplete"), 
+    //                             '3082d9f8f08846a987a1c0a3ed4ecd05', 
+    //                             {  });
+    //     autocompleteInput.on('select', (location) => {
+    //     const placeName = location.properties.address_line1;
+    //     const placeLocation = location.properties.address_line2;
+    //     const latitude = location.properties.lat;
+    //     const longitude = location.properties.lon;
+
+    //     if (!placeName || !placeLocation || !latitude || !longitude) {
+    //         alert("Invalid address. Please select a valid address.");
+    //         return;
+    //     }
+
+    //     // Set the selected location values to hidden inputs in the form
+    //     document.getElementById('address_1').value = placeName;
+    //     document.getElementById('address_2').value = placeLocation;
+    //     document.getElementById('latitude').value = latitude;
+    //     document.getElementById('longitude').value = longitude;
+    //     });
+    // }
+    // const inputs = document.getElementsByClassName("autocomplete-container");
+
+    // Array.from(inputs).forEach((input) => {
+    //   const autocompleteInput = new autocomplete.GeocoderAutocomplete(input, '3082d9f8f08846a987a1c0a3ed4ecd05');
+
+    //   autocompleteInput.on('select', (location) => {
+    //         const placeName = location.properties.address_line1;
+    //         const placeLocation = location.properties.address_line2;
+    //         const latitude = location.properties.lat;
+    //         const longitude = location.properties.lon;
+    
+    //         if (!placeName || !placeLocation || !latitude || !longitude) {
+    //             alert("Invalid address. Please select a valid address.");
+    //             return;
+    //         }
+    
+    //         // Set the selected location values to hidden inputs in the form
+    //         document.getElementById('address_1').value = placeName;
+    //         document.getElementById('address_2').value = placeLocation;
+    //         document.getElementById('latitude').value = latitude;
+    //         document.getElementById('longitude').value = longitude;
+    //         });
+    // });
+
+    function initializeAutocomplete(input) {
+      const autocompleteInput = new autocomplete.GeocoderAutocomplete(input, '3082d9f8f08846a987a1c0a3ed4ecd05');
+    
+      autocompleteInput.on('select', (location) => {
         const placeName = location.properties.address_line1;
         const placeLocation = location.properties.address_line2;
         const latitude = location.properties.lat;
         const longitude = location.properties.lon;
-
+    
         if (!placeName || !placeLocation || !latitude || !longitude) {
-            alert("Invalid address. Please select a valid address.");
-            return;
+          alert("Invalid address. Please select a valid address.");
+          return;
         }
-
+        // console.log(input.parentNode.querySelector('#address_2').id, placeName)
         // Set the selected location values to hidden inputs in the form
-        document.getElementById('address_1').value = placeName;
-        document.getElementById('address_2').value = placeLocation;
-        document.getElementById('latitude').value = latitude;
-        document.getElementById('longitude').value = longitude;
-        });
+        input.parentNode.querySelector('#address_1').value = placeName;
+        input.parentNode.querySelector('#address_2').value = placeLocation;
+        input.parentNode.querySelector('#latitude').value = latitude;
+        input.parentNode.querySelector('#longitude').value = longitude;
+      });
     }
+    
+    const inputs = document.getElementsByClassName("autocomplete-container");
+    
+    Array.from(inputs).forEach((input) => {
+      initializeAutocomplete(input);
+    });
+    
 
 
   });
--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -6,18 +6,17 @@
                 <div class="flex mb-4">
                     <div class="w-1/3 mr-2">
                         <label for="cn" class="block text-sm font-medium text-gray-700">CN No</label>
-                        <input id="cn" type="text" class="h-10 bg-zinc-500 text-white  mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md" value="{{ generateBookingCode() }}" readonly>
+                        <input id="cn" type="text" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md" value="{{ generateBookingCode() }}" readonly>
                     </div>
                     <div class="w-1/3 mx-2">
                         <label for="date" class="block text-sm font-medium text-gray-700">Date</label>
-                        <input id="date" type="date" class="h-10  mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                        <input id="date" type="date" class="h-10 px-2  mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                     </div>
                     <div class="w-1/3 ml-2">
                         <label for="category" class="block text-sm font-medium text-gray-700">Category</label>
-                        <select id="category" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
-                            <option>Option 1</option>
-                            <option>Option 2</option>
-                            <option>Option 3</option>
+                        <select id="category" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <option value="Domestic">Domestic</option>
+                            <option value="International">International</option>
                         </select>
                     </div>
                 </div>
@@ -25,11 +24,11 @@
                     <label class="block text-sm font-medium text-gray-700">Payment Mode</label>
                     <div class="mt-1">
                         <label class="inline-flex items-center">
-                        <input type="radio" class="h-10 form-radio text-indigo-500 focus:ring-indigo-500" name="payment-mode" value="cash">
+                        <input type="radio" class="h-10 px-2 form-radio text-indigo-500 focus:ring-indigo-500" name="payment-mode" value="cash">
                             <span class="ml-2">Cash</span>
                         </label>
                         <label class="inline-flex items-center ml-6">
-                        <input type="radio" class="h-10 form-radio text-indigo-500 focus:ring-indigo-500" name="payment-mode" value="credit">
+                        <input type="radio" class="h-10 px-2 form-radio text-indigo-500 focus:ring-indigo-500" name="payment-mode" value="credit">
                             <span class="ml-2">Credit</span>
                         </label>
                     </div>
@@ -39,55 +38,63 @@
                         <div class="grid grid-cols-2 gap-2">
                             <div>
                                 <label for="shipper" class="block text-sm font-medium text-gray-700">Shipper</label>
-                                <select id="shipper" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
-                                    <option>Option 1</option>
-                                    <option>Option 2</option>
-                                    <option>Option 3</option>
+                                <select id="shipper" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                                    <option>Choose Shipper</option>
+                                    @foreach (\App\Models\Shipper::all() as $shipper)
+                                    <option
+                                        value="{{ $shipper->id }}"
+                                        {{ old('shipper_id') == $shipper->id ? 'selected' : '' }}
+                                    >{{ ucwords($shipper->name) }}</option>
+                                @endforeach
                                 </select>
                             </div>
                             <div>
-                                <input id="new_shipper" type="text" class="h-10 mt-6  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                                <input id="new_shipper" type="text" placeholder="Create new shipper" class="h-10 px-2 mt-6  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                             </div>
                             <div>
                                 <label for="shipper_number" class="block text-sm font-medium text-gray-700">Number</label>
-                                <input id="shipper_number" type="text" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                                <input id="shipper_number" type="text" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                             </div>
                             <div>
-                                <label for="shipper_city" class="block text-sm font-medium text-gray-700">City</label>
-                                <input id="shipper_city" type="text" class="h-10  mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
-                            </div>
-                            <div>
-                                <label for="shipper_location" class="block text-sm font-medium text-gray-700">Location</label>
-                                <input id="shipper_location" type="text" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                                <label for="shipper_address" class="block text-sm font-medium text-gray-700">Address</label>
+                                <div id="autocomplete" class="autocomplete-container billing"></div>
+                                <input type="hidden" name="address_1" id="address_1" >
+                                <input type="hidden" name="address_2" id="address_2" >
+                                <input type="hidden" name="longitude" id="longitude" >
+                                <input type="hidden" name="latitude" id="latitude" >
                             </div>
                         
                         </div>
                     </div>
                     <div class="w-1/2 ml-2">
                         <div class="grid grid-cols-2 gap-2">
-                        <div>
-                            <label for="consignee_name" class="block text-sm font-medium text-gray-700">Consignee Name</label>
-                            <select id="consignee" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
-                                <option>Option 1</option>
-                                <option>Option 2</option>
-                                <option>Option 3</option>
-                            </select>
-                        </div>
-                        <div>
-                            <input id="new_consignee" type="text" class="h-10 mt-6  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
-                        </div>
-                        <div>
-                            <label for="consignee_number" class="block text-sm font-medium text-gray-700">Mobile Number</label>
-                            <input id="consignee_number" type="text" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
-                        </div>
-                        <div>
-                            <label for="consignee_city" class="block text-sm font-medium text-gray-700">City</label>
-                            <input id="consignee_city" type="text" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
-                        </div>
-                        <div>
-                            <label for="consignee_location" class="block text-sm font-medium text-gray-700">Location</label>
-                            <input id="consignee_location" type="text" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
-                        </div>
+                            <div>
+                                <label for="consignee" class="block text-sm font-medium text-gray-700">Shipper</label>
+                                <select id="consignee" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                                    <option>Choose Consignee</option>
+                                    @foreach (\App\Models\Consignee::all() as $consignee)
+                                    <option
+                                        value="{{ $consignee->id }}"
+                                        {{ old('consignee_id') == $consignee->id ? 'selected' : '' }}
+                                    >{{ ucwords($consignee->name) }}</option>
+                                @endforeach
+                                </select>
+                            </div>
+                            <div>
+                                <input id="new_consignee" type="text" placeholder="Create new consignee" class="h-10 px-2 mt-6  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            </div>
+                            <div>
+                                <label for="consignee_number" class="block text-sm font-medium text-gray-700">Number</label>
+                                <input id="consignee_number" type="text" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            </div>
+                            <div>
+                                <label for="consignee_address" class="block text-sm font-medium text-gray-700">Address</label>
+                                <div id="autocomplete" class="autocomplete-container billing"></div>
+                                <input type="hidden" name="address_1" id="address_1" >
+                                <input type="hidden" name="address_2"  id="address_2" >
+                                <input type="hidden" name="longitude"  id="longitude" >
+                                <input type="hidden" name="latitude"  id="latitude" >
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -95,7 +102,7 @@
                     <div class="grid grid-cols-5 gap-2">
                         <div>
                             <label for="content_type" class="block text-sm font-medium text-gray-700">Content Type</label>
-                            <select id="content_type" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <select id="content_type" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                                 <option>Option 1</option>
                                 <option>Option 2</option>
                                 <option>Option 3</option>
@@ -103,7 +110,7 @@
                         </div>
                         <div>
                             <label for="merchandise_type" class="block text-sm font-medium text-gray-700">Merchandise Type</label>
-                            <select id="merchandise_type" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <select id="merchandise_type" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                                 <option>Option 1</option>
                                 <option>Option 2</option>
                                 <option>Option 3</option>
@@ -111,7 +118,7 @@
                         </div>
                         <div>
                             <label for="mode" class="block text-sm font-medium text-gray-700">Mode</label>
-                            <select id="mode" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <select id="mode" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                                 <option>Option 1</option>
                                 <option>Option 2</option>
                                 <option>Option 3</option>
@@ -119,11 +126,11 @@
                         </div>
                         <div>
                             <label for="quantity" class="block text-sm font-medium text-gray-700">Quantity</label>
-                            <input id="quantity" type="number" value="0" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <input id="quantity" type="number" value="0" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                         </div>
                         <div>
                             <label for="weight" class="block text-sm font-medium text-gray-700">Weight</label>
-                            <input id="weight" type="number" value="0" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <input id="weight" type="number" value="0" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                         </div>
                     </div>
                 </div>
@@ -131,23 +138,23 @@
                     <div class="grid grid-cols-5 gap-2">
                         <div>
                             <label for="individual_price" class="block text-sm font-medium text-gray-700">Individual Price</label>
-                            <input id="individual_price" value="0" type="text" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <input id="individual_price" value="0" type="text" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                         </div>
                         <div>
                             <label for="price-before-discount" class="block text-sm font-medium text-gray-700">Price Before Discount</label>
-                            <input id="price-before-discount" value="0" type="h-10 text" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <input id="price-before-discount" value="0" type="h-10 px-2 text" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                         </div>
                         <div>
                             <label for="discount" class="block text-sm font-medium text-gray-700">Discount</label>
-                            <input id="discount" type="text" value="0" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <input id="discount" type="text" value="0" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                         </div>
                         <div>
                             <label for="price-after-discount" class="block text-sm font-medium text-gray-700">Price After Discount</label>
-                            <input id="price-after-discount" value="0" type="text" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <input id="price-after-discount" value="0" type="text" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                         </div>
                         <div>
                             <label for="biller" class="block text-sm font-medium text-gray-700">Biller</label>
-                            <input id="biller" type="text" class="h-10 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
+                            <input id="biller" type="text" class="h-10 px-2 mt-1  block w-full shadow-md sm:text-sm border-gray-300 rounded-md">
                         </div>
                     </div>
                 </div>
@@ -158,7 +165,7 @@
                     </div>
                 </div>
                 <div class="flex justify-center">
-                    <button type="submit" class="h-10  px-4 py-2 text-sm font-medium text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 rounded-md">Save Booking</button>
+                    <button type="submit" class="h-10 px-2  px-4 py-2 text-sm font-medium text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 rounded-md">Save Booking</button>
                 </div>
             </form>
         </div>
