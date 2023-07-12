15aacfc3f30d88383b3dc2495b921fa302813eb1
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-07-12 16:33:53 +0545

    added jquery support

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,11 +1,13 @@
 <!doctype html>
 
 <title>CMS</title>
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="stylesheet" href="{{ URL::asset ('css/app.css') }}">
 <link rel="stylesheet" href="{{ URL::asset ('css/vendor/fontawesome/css/all.min.css') }}">
 <link rel="stylesheet" href="https://unpkg.com/@geoapify/geocoder-autocomplete@^1/styles/minimal.css">
 @vite('resources/css/app.css')
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="{{ URL::asset ('js/jquery-3.7.0.min.js') }}"></script>
 <script src="{{ URL::asset ('js/vendor/alpine/alpine.min.js') }}"></script>
 <script src="{{ URL::asset ('js/custom.js') }}"></script>
 <script src="https://unpkg.com/@geoapify/geocoder-autocomplete@^1/dist/index.min.js"></script>
