9a1adb74308723ae5101cd2f687f7c8b34128c10
RajaramBhurtel <sabinbhurtel@gmail.com>
2023-07-16 20:07:38 +0545

    basic bill structure

--- resources/views/booking/view.blade.php
+++ resources/views/booking/view.blade.php
@@ -1,116 +1,125 @@
 <x-layout >
     <x-main title="View ">
         <x-panel>
-            <div class="flex mb-40 w-700 h-400 ml-10">
-                <div class="h-396 w-500" >
-                  <div class="flex" style="margin-top: 5px; border:2px solid black">
-                    <img alt="346897" src="#" class="h-80 ml-5">
-                    <div class="ml-10">
-                      <h3 class="ml-95 mt-75">Company Name</h3>
-                      <hr class="border-dotted  w-380 ml-95 mt-5">
-                      <p class="ml-95 mt-10">
-                        Address and number
-                      </p>
-                      <p class="ml-200 mt-5">
-                        <span>Website</span>
-                      </p>
-                    </div>
-                  </div>
-                  <hr class=" w-486 ml-n2 mt-n3">
-                  <div class="w-485 h-120 mt-n20  ">
-                    <table class="table" style="margin-top: 5px; border:2px solid black">
-                      <tbody>
-                        <tr>
-                          <td>
-                            <span class="text-sm">
-                              Origin: <br>
-                              Sender: <b class="text-base">
-                                Name
-                              </b>
-                              <br>
-                              Number: <b class="text-base">Number</b><br>
-                              Address: <b class="text-base">Bhaktapur</b><br>
-                              Booked On: <b class="text-base">2080-3-29 BS<b>
-                              </b></b>
-                            </span>
-                          </td>
-                          <td>
-                            <span class="text-sm">
-                              Receiver: <b class="text-base">
-                                RAM KRISHNA KAMI
-                              </b>
-                              <br>
-                              Number: <b class="text-base">+911567702876</b><br>
-                              Address: <b class="text-base">United Arab Amirates, Ajman, New Sanaya UAE</b>
-                            </span>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                  <span class="ml-8 text-sm">Description: <b class="text-base"></b>&nbsp;&nbsp;</span>
-                  <table class=" mt-10 ml-5 border-2" style="border: 1px solid black;width: 475px;">
-                    <tbody>
-                      <tr>
-                        <td class="text-center">Mailing Mode</td>
-                        <td class="text-center"><b class="text-base">Air</b></td>
-                        <td class="text-center">Payment Mode</td>
-                        <td class="text-center"><b class="text-base">Cash</b></td>
-                      </tr>
-                      <tr>
-                        <td class="text-center">Declared Value</td>
-                        <td class="text-center"><b class="text-base">0</b></td>
-                        <td class="text-center">Quantity</td>
-                        <td class="text-center"><b class="text-base">1</b></td>
-                      </tr>
-                      <tr>
-                        <td class="text-center">Content</td>
-                        <td class="text-center"><b class="text-base">Doc</b></td>
-                        <td class="text-center">Weight</td>
-                        <td class="text-center"><b class="text-base">0.5</b></td>
-                      </tr>
-                      <tr>
-                        <td colspan="2">&nbsp;Booked On: <b class="text-base">2023-07-14 AD</b></td>
-                        <td>Amount: <b class="text-base">Rs 2800</b></td>
-                        <td class="text-right">&nbsp;Don't Put Cash in Envelope</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <p class="ml-8 text-sm mt-10 ">
-                    Received with thanks NRs <b class="text-base">2800</b>. In Words Two Thousand Eight Hundred Only
+            
+          <div class="flex max-w-4xl mx-auto my-10 border-2 border-black">
+            <div class="left border-r-2 border-black w-3/4">
+              <div class="flex top border-b border-black">
+                <img class="mt-3 h-20 w-20" src="{{ URL::asset ('logo.png') }}" alt="EICCS"/>
+                <div class="p-3 text-center">
+                  <h3 class="ml-95 text-2xl font-bold">Everest Innovative Courier & Cargo Service</h3>
+                  <hr class="border-dotted border-black w-380 ml-95 mt-1">
+                  <p class="ml-95 mt-2">
+                    Kalimati , Kathmandu , Nepal | Tel : 01-5244145 , 01-5244257
+                  </p>
+                  <p class="ml-200 mt-1">
+                    <span>www.eiccs.com.np</span>
                   </p>
                 </div>
-                <div class="  w-30 h-396 mt-n400 float-right">
-                  <div class="  h-140">
-                    <p class="text-xs text-center">
-                      <b>Courier Consignment <br>Note Non Negotiable</b>
-                    </p>
-                    <p class="text-center"></p>
-                    <div class="ml-15 p-0 overflow-auto w-176">
-                      Barcode
-                    </div>
-                    <div id="tc">
-                      <p class="text-center mt-5 text-xs">
-                        <b>TERMS AND CONDITIONS:</b>
-                      </p>
-                      <p class="ml-8 text-xs mt-n10">
-                        Unless a greater value is declared in writing in the space provided on this airway bill and freight on
-                        value charges are paid, the shipper declares and agrees that the declared value of the shipment is up to
-                        Rs.100, the shipper agrees that he has read and understands the terms and conditions set on that shipment
-                        does not contain any currency or any material which violates the conditions of carriage
-                      </p>
-                      <hr class=" mt-1">
-                      <p class="text-center mt-n10">
-                        <b>Biller</b><br>
-                        ----------------------------------------<br>
-                        <span>
-                          <b>For: Company</b>
+              </div>
+              <hr class="border-solid border-black w-486 ml-n2 mt-n2">
+              <div class="bottom  border-black">
+                <table class="table m-8">
+                  <tbody>
+                    <tr >
+                      <td class="w-1/2">
+                        <span class="text-sm">
+                          {{-- Origin: <br> --}}
+                          Sender: <b class="text-base">Sabin Bhurtel </b>
+                          <br>
+                          Number: <b class="text-base">Number</b><br>
+                          Address: <b class="text-base">Bhaktapur</b><br>
+                          Booked On: <b class="text-base">2080-3-29 BS<b>
+                          </b></b>
                         </span>
-                      </p>
-                    </div>
-                  </div>
+                      </td>
+                      <td class="w-1/2">
+                        <span class="text-sm">
+                          Receiver: <b class="text-base">
+                            RAM KRISHNA KAMI
+                          </b>
+                          <br>
+                          Number: <b class="text-base">+911567702876</b><br>
+                          Address: <b class="text-base">United Arab Amirates, Ajman, New Sanaya UAE</b>
+                        </span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <hr class="mb-4 border-black">
+
+                <span class="ml-8 mb-2 text-sm">Description: <b class="text-base"></b>&nbsp;&nbsp;</span>
+                <table class=" border-solid border-black mt-2 ml-5 border-2 min-w-[95%]" >
+                  <tbody>
+                    <tr>
+                      <td class="text-center border p-2 border-black">Mailing Mode</td>
+                      <td class="text-center border p-2 border-black"><b class="text-base">Air</b></td>
+                      <td class="text-center border p-2 border-black">Payment Mode</td>
+                      <td class="text-center border p-2 border-black"><b class="text-base">Cash</b></td>
+                    </tr>
+                    <tr>
+                      <td class="text-center border p-2 border-black">Declared Value</td>
+                      <td class="text-center border p-2 border-black"><b class="text-base">0</b></td>
+                      <td class="text-center border p-2 border-black">Quantity</td>
+                      <td class="text-center border p-2 border-black"><b class="text-base">1</b></td>
+                    </tr>
+                    <tr>
+                      <td class="text-center border p-2 border-black">Content</td>
+                      <td class="text-center border p-2 border-black"><b class="text-base">Doc</b></td>
+                      <td class="text-center border p-2 border-black">Weight</td>
+                      <td class="text-center border p-2 border-black"><b class="text-base">0.5</b></td>
+                    </tr>
+                    <tr>
+                      <td class="text-center border p-2 border-black">Booked On:</td>
+                      <td class="text-center border p-2 border-black"><b class="text-base">2023-07-14 AD</b></td>
+                      <td class="text-center border p-2 border-black">Amount:</td>
+                      <td class="text-center border p-2 border-black"><b class="text-base">Rs 2800</b></td>
+                    </tr>
+                    <tr>
+                      <td colspan="4" class="text-center  border p-2 border-black">&nbsp;Don't Put Cash in Envelope</td>
+
+                    </tr>
+                  </tbody>
+                </table>
+                <p class="ml-8 text-sm mt-3 mb-3">
+                  Received with thanks NRs <b class="text-base">2800</b>. In Words Two Thousand Eight Hundred Only
+                </p>
+              </div>
+            </div>
+            <div class="right w-1/4">
+              <div class="top border-b border-black">
+                <p class="text-xs text-center border p-2">
+                  <b>Courier Consignment <br>Note Non Negotiable</b>
+                </p>
+                <p class="text-center"></p>
+                <div class="ml-15 p-0 overflow-auto w-176">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlcyf1kJNUTZqLcHaLsR_gKfuoaumFl-Sd0m1Lgd3ADg&s" alt="">
+                  18875
                 </div>
               </div>
+              <div class="bottom ">
+                <p class="text-center mt-5 text-xs">
+                  <b>TERMS AND CONDITIONS:</b>
+                </p>
+                <p class="ml-8 text-xs mt-1">
+                  Unless a greater value is declared in writing in the space provided on this airway bill and freight on
+                  value charges are paid, the shipper declares and agrees that the declared value of the shipment is up to
+                  Rs.100, the shipper agrees that he has read and understands the terms and conditions set on that shipment
+                  does not contain any currency or any material which violates the conditions of carriage
+                </p>
+                <hr class="border-solid border-black mt-4">
+                <p class="text-center mt-4">
+                  <b class="mt-2">Sabin Bhurtel</b>
+                  <br><br><br>
+                  ----------------------------------------<br>
+                  <span>
+                    <b>For: EICCS</b>
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+
               
             
         </x-panel>      
