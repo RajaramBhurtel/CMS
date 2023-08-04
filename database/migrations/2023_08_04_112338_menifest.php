391742a86564d196a7bc065aa9b4d79d0fa25dfa
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-08-04 17:24:45 +0545

    menifest table

---
+++ database/migrations/2023_08_04_112338_menifest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menifests', function (Blueprint $table) {
+            $table->id();
+            $table->string('menifests_code')->unique();
+            $table->string('date');
+            $table->string('location');
+            $table->string('cn_no');
+            $table->string('consignee');
+            $table->string('address');
+            $table->string('contact');
+            $table->string('booked_on');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menifests');
+    }
+};
